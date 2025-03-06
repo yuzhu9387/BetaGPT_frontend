75468c04dc499929f8b3af8387581f30a5b1f86f
Xixi_Guo <yuzhu9387@gmail.com>
2025-03-06 15:57:53 -0800

    update app name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>BetaLink</h1>
+        <h1>BetaGPT</h1>
         <h2>Instantly find anyone in your network.</h2>
       </header>
       <main>
